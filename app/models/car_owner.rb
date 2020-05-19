b8dcab13490c20f093fc26805145fbe01a452fff
emilioquintana90 <emilioquintana90@gmail.com>
2020-05-19 08:45:26 -0500

    Done.

--- app/models/car_owner.rb
+++ app/models/car_owner.rb
@@ -1,10 +1,26 @@
 class CarOwner
-
+  @@all = []
   attr_reader :name
 
   def initialize(name)
     @name = name
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
+
+  def cars
+    Car.all.select {|c| c.car_owner == self}
+  end
+
+  def mechanics
+    cars.map {|c| c.mechanic}
+  end
+
+  def self.average_number_of_cars
+    (self.all.sum {|co| co.cars.length}) / self.all.count
+  end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -4,4 +4,21 @@ def reload
   load 'config/environment.rb'
 end
 
+# ******** CAR OWNER INSTANCES *********
+emilio = CarOwner.new("Emilio")
+dieter = CarOwner.new("Dieter")
+
+# ******** MECHANIC INSTANCES *********
+billy = Mechanic.new("Billy", "Motors")
+romulo = Mechanic.new("Romulo", "Motors")
+
+# ******** CAR INSTANCES *********
+Car.new(emilio, billy, "Ford", "Fiesta", "sedan")
+Car.new(emilio, romulo, "Lambo", "Murcielago", "premium")
+
+Car.new(dieter, billy, "Hyundai", "Sonata", "sedan")
+Car.new(dieter, romulo, "Ford", "Fiesta", "sedan")
+
+
+
 binding.pry
